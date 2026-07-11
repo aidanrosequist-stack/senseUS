26462da6adfec885f5010933a0ee91bf6fd960d3
Aidan Rosequist <aidan.rosequist@gmail.com>
2026-07-11 02:53:00 -0400

    Add branded 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Mission from './pages/Mission'
 import HowItWorks from './pages/HowItWorks'
 import QuestionPreview from './pages/QuestionPreview'
 import Transparency from './pages/Transparency'
+import NotFound from './pages/NotFound'
 import Ethos from './pages/Ethos'
 
 function App() {
@@ -41,6 +42,7 @@ function App() {
         <Route path="/q/:number" element={<QuestionPreview />} />
         <Route path="/transparency" element={<Transparency />} />
         <Route path="/ethos" element={<Ethos />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
